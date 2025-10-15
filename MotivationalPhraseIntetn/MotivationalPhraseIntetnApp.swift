3b379079c735cb4628e1cfe6aee38db514b32134
vitor <viktorbruno2006@gmail.com>
2025-10-15 14:30:47 -0300

    Initial Commit

---
+++ MotivationalPhraseIntetn/MotivationalPhraseIntetnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationalPhraseIntetnApp.swift
+//  MotivationalPhraseIntetn
+//
+//  Created by Vítor Bruno on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MotivationalPhraseIntetnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotivationalPhraseIntetnTests/MotivationalPhraseIntetnTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationalPhraseIntetnTests.swift
+//  MotivationalPhraseIntetnTests
+//
+//  Created by Vítor Bruno on 15/10/25.
+//
+
+import Testing
+@testable import MotivationalPhraseIntetn
+
+struct MotivationalPhraseIntetnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
